cb54fbee033260f4ea363b8e2a8da24347c73b95
Ved54 <vedantvvaidya3@gmail.com>
2025-05-10 02:49:05 +0000

    Initial commit with Java project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, SonarQube!");
+    }
+
+    // A method with a dummy issue for SonarQube to detect
+    public String unusedMethod() {
+        String unusedVar = "This is unused";
+        return "Hello";
+    }
+}
